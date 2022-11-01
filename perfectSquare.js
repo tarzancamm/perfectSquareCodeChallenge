0dc594efc99869053d22eaf0ebf07809293922bb
Cameron Miner <cameronsminer@gmail.com>
2022-11-01 10:17:48 -0600

    Created second function that accepts any number and returns next highest square

--- perfectSquare.js
+++ perfectSquare.js
@@ -15,11 +15,23 @@ const perfectSquare = (num) => {
     }
 }
 
-
 console.log(perfectSquare(9)) // should return 16 (3x3=9, 4x4=16)
-
 console.log(perfectSquare(25)) // should return 36
-
 console.log(perfectSquare(289)) // should return 324 (17x17=289 18x18=324)
-
 console.log(perfectSquare(3000)) // should return -1 (sq root of 3000 is 54.77)
+
+
+
+// Challenge 2: create function that accepts any number and returns next highest square
+
+const nextPerf = (num) => {
+    if (num > 0) {
+        return Math.pow(Math.floor(Math.sqrt(num)) + 1, 2)
+    } else {
+        return 0
+    }
+}
+
+console.log(nextPerf(9.82)) // should return 16
+console.log(nextPerf(289)) //should return 324
+console.log(nextPerf(-1)) // should return 0
